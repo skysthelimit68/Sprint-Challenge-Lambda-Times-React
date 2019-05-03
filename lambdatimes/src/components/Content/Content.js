a3a737881ab6fd2d057a2d6623580da9a5c8f6f5
Ming Liu <mliu@macpro.home>
2019-05-03 11:30:20 -0400

    contents tabs and starting on tab

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -12,16 +12,25 @@ export default class Content extends Component {
     this.state = {
       selected: 'all',
       tabs: [],
-      cards: []
+      cards: [],
+      filteredCards: []
     };
   }
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs : tabData,
+      cards : cardData,
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected : tab
+    })
+
   };
 
   filterCards = () => {
@@ -37,7 +46,15 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    let tempFilter = this.state.cards.filter( card => {
+        card.tab === this.state.selected
+    })
+
+    //this.setState({
+    //  filteredCards : tempFilter
+    //})
+
+    return tempFilter;
   };
 
   render() {
@@ -48,7 +65,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
