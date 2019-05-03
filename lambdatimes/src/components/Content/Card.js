d2e67f858f28f6a07d8d79ecca7d36ee284203ea
Ming Liu <mliu@macpro.home>
2019-05-03 13:02:20 -0400

    refactor to all styled components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const S_Card = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;`;
+
+const S_CardHeadline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;`;
+
+const S_CardAuthor = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;`;
+
+const S_CardImgContainer = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;`;
+
+const S_Img = styled.img`
+  width: 40px;
+`;
+
+const S_CardSpan = styled.span`
+  padding-left: 10px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline /* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={props.card.img /* image source goes here */} />
-        </div>
-        <span>By {props.card.author/* author goes here */}</span>
-      </div>
-    </div>
+    <S_Card>
+      <S_CardHeadline>{props.card.headline}</S_CardHeadline>
+      <S_CardAuthor>
+        <S_CardImgContainer>
+          <S_Img src={props.card.img}></S_Img>
+        </S_CardImgContainer>
+        <S_CardSpan>By {props.card.author}</S_CardSpan>
+      </S_CardAuthor>
+    </S_Card>
   );
 };
 
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const S_Tab = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  ${props => (props.type === 'active-tab' ? `background-color: #fff; color: #333; border: 2px solid #333;` : null)}
+`;
+
 
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  let tabClass = props.tab === props.selectedTab ? "tab active-tab" : "tab";
+  let tabClass = props.tab === props.selectedTab ? "active-tab" : null;
   
   return (
-    <div
-      className={tabClass}
+    <S_Tab
+      type={tabClass}
       onClick = {() => {props.selectTabHandler(props.tab)}}
       /*onClick={() => {
         Replace this dummy click handler function with your selectTabHandler function from props 
@@ -20,7 +43,7 @@ const Tab = props => {
       }}*/
     >
       {props.tab.toUpperCase()}
-    </div>
+    </S_Tab>
   );
 };
 
